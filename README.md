abdcd3e45ccc8e319441ced471c253d6601de048
Taylor Pool <taylorpool.27@gmail.com>
2024-04-05 13:48:03 -0400

    Parallel summation

---
+++ README.md
@@ -0,0 +1,6 @@
+# Parallelism Tutorial
+
+## Build and Run
+1. Clone this repository.
+2. nix-build
+3. ./result/bin/main
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,21 @@
 #include <iostream>
 #include <vector>
 
+struct Timer {
+  decltype(std::chrono::system_clock::now()) m_start;
+
+  [[nodiscard]] Timer() : m_start(std::chrono::system_clock::now()) {}
+
+  ~Timer() {
+    const auto end = std::chrono::system_clock::now();
+    std::cout << "Took "
+              << std::chrono::duration_cast<std::chrono::nanoseconds>(end -
+                                                                      m_start)
+                     .count()
+              << " nanoseconds\n";
+  }
+};
+
 void print_vector(const std::vector<double> vec) {
   for (const auto x : vec) {
     std::cout << x << ", ";
@@ -14,55 +29,61 @@ void print_vector(const std::vector<double> vec) {
 int main(int argc, char *argv[]) {
 
   // We will start with this vector
-  std::vector<double> vec0{0.0, 1.0, 2.0, 3.0};
+  std::vector<double> vec0{0.0, 1.0, 2.0, 3.0, 4.0, 5.0,
+                           6.0, 7.0, 8.0, 9.0, 10.0};
 
   std::cout << "original vector\n";
   print_vector(vec0);
   std::cout << "\n";
 
+  // Transform each element sequentially and time it!
+  // std::transform transforms a container that has a begin and a end iterator
+  // (can think of these as pointers).
+  // The begin iterator of the output range also has to be passed in. Note
+  // that std::transform does not do any allocation, so the output vector
+  // already has to have the correct number of allocated elements prior to
+  // passing to std::transform. Otherwise, you can use std::back_inserter and
+  // it will allocate as you go. But using std::back_inserter is naturally
+  // slower
+  std::cout << "sequentially transforming vector\n";
   {
-    const auto start = std::chrono::system_clock::now();
-
-    // Transform each element sequentially and time it!
-    // std::transform transforms a container that has a begin and a end iterator
-    // (can think of these as pointers).
-    // The begin iterator of the output range also has to be passed in. Note
-    // that std::transform does not do any allocation, so the output vector
-    // already has to have the correct number of allocated elements prior to
-    // passing to std::transform. Otherwise, you can use std::back_inserter and
-    // it will allocate as you go. But using std::back_inserter is naturally
-    // slower
+    Timer timer;
     std::transform(vec0.begin(), vec0.end(), vec0.begin(),
                    [](double &x) { return x + 1; });
-    const auto end = std::chrono::system_clock::now();
-
-    std::cout << "sequentially transformed vector\n";
-    std::cout << "took "
-              << std::chrono::duration_cast<std::chrono::nanoseconds>(end -
-                                                                      start)
-                     .count()
-              << " nanoseconds \n";
-    print_vector(vec0);
-    std::cout << "\n";
   }
+  print_vector(vec0);
+  std::cout << "\n";
 
+  // Transform each element in parallel unsequenced and time it! Unsequenced
+  // means it does not matter which element is transformed first.
+  std::cout << "parallel unsequence transforming vector\n";
   {
-    const auto start = std::chrono::system_clock::now();
-
-    // Transform each element in parallel unsequenced and time it! Unsequenced
-    // means it does not matter which element is transformed first.
+    Timer timer;
     std::transform(std::execution::par_unseq, vec0.begin(), vec0.end(),
                    vec0.begin(), [](double &x) { return x + 1; });
-    const auto end = std::chrono::system_clock::now();
+  }
+  print_vector(vec0);
+  std::cout << "\n";
 
-    std::cout << "parallel unsequenced transformed vector\n";
-    std::cout << "took "
-              << std::chrono::duration_cast<std::chrono::nanoseconds>(end -
-                                                                      start)
-                     .count()
-              << " nanoseconds \n";
-    print_vector(vec0);
-    std::cout << "\n";
+  std::cout << "sequential summing vector\n";
+  {
+    double tmp;
+    {
+      Timer timer;
+      tmp = std::reduce(vec0.cbegin(), vec0.cend(), 0.0, std::plus<double>());
+    }
+    std::cout << "sum: " << tmp << "\n\n";
+  }
+
+  std::cout << "parallel unsequence summing vector\n";
+  {
+    double tmp;
+    {
+      Timer timer;
+      tmp = std::reduce(std::execution::par_unseq, vec0.cbegin(), vec0.cend(),
+                        0.0, std::plus<double>());
+    }
+    std::cout << "sum: " << tmp << "\n\n";
   }
 
   return 0;
